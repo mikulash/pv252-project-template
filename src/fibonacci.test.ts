5a3e19450624488515e0eced0c0ec24170f8bfa4
Mikulash <62067494+mikulash@users.noreply.github.com>
2024-09-27 20:12:39 +0200

    improve code coverage.

--- src/fibonacci.test.ts
+++ src/fibonacci.test.ts
@@ -3,3 +3,15 @@ import { fibonacci } from "./fibonacci.ts";
 test("fibonacci-5", () => {
   expect(fibonacci(5)).toBe(5);
 });
+
+test("fibonacci-0", () => {
+  expect(fibonacci(0)).toBe(0);
+});
+
+test("fibonacci-1", () => {
+  expect(fibonacci(1)).toBe(1);
+});
+
+test("fibonacci-negative", () => {
+  expect(() => fibonacci(-1)).toThrow("Cannot compute on negative numbers");
+});
